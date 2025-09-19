348131c27c5733e8b9e2ab9e14543f17d5891802
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-09-19 15:19:55 +0200

    Feat: - Add Header - Improve UI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import { NavLink } from "react-router-dom"
 
 const Header = () => {
     return (
-        <div>
-
-        </div>
+        <header>
+            <nav>
+                <ul className="flex p-4 gap-4">
+                    <li>
+                        <NavLink
+                            to='/'
+                            className='hover:brightness-150'
+                        >Lista Tasks</NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/add-task'
+                            className='hover:brightness-150'
+                        >Aggiungi Task</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
